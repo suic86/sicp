cf9e0516cf67b0665e218efdb5a7caea5dc84223
suic86 <marko.gabriel.cz@gmail.com>
2025-12-12 19:48:12 +0100

    Use org file.

--- factorial.scm
+++
@@ -1,6 +0,0 @@
-(define (factorial n)
-  (define (factorial-iter total counter)
-    (if (= counter n)
-        total
-        (factorial-iter (* total counter) (+ counter 1))))
-  (factorial-iter 1 1))
