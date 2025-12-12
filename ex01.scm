3b7554e3b4c2dde0d43c31fbbba4a225091ba513
suic86 <marko.gabriel.cz@gmail.com>
2025-12-12 19:13:04 +0100

    Exercise 1.15 - code.

--- ex01.scm
+++ ex01.scm
@@ -204,3 +204,16 @@
 ;; Exercise 1.13
 ;;
 ;; Skipped for now.
+
+;; Exercise 1.14
+;;
+;; Skipped for now.
+
+;; Exercise 1.15
+
+;; (define (cube x) (* x x x)) -- define above
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
